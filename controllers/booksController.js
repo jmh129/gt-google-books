859a147dad9b8e90c0eba9cd8d168792cb0ea036
James Holding <james.holding90@gmail.com>
2020-08-10 20:59:47 -0400

    rebase

---
+++ controllers/booksController.js
@@ -0,0 +1,45 @@
+const db = require("../models/books");
+const axios = require("axios");
+
+// Defining methods for the booksController
+module.exports = {
+  findAll: function (req, res) {
+    db.Book.find(req.query)
+      .sort({ date: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  searchGoogle: function (req, res) {
+    const query = req.body;
+    console.log(query);
+    axios
+      .get(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  findById: function (req, res) {
+    db.Book.findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.Book.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.Book.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    db.Book.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+};
---
+++ models/books.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  description: String,
+  date: { type: Date, default: Date.now },
+  imageLink: { type: String },
+  buyLink: { type: String }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
