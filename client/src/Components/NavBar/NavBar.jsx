c002dbc7906b3a567608c8092dfb9ac03f1fa4f5
James Holding <james.holding90@gmail.com>
2020-08-10 21:48:31 -0400

    added navbar.

---
+++ client/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
+const Navigation = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">GoogleBookSearch</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Search</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/saved">
+                Saved
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navigation;
